7fe998a2c8bb7ca65295e3a066eb1f6d0dafabf2
Ivan <ivansdk707@gmail.com>
2023-08-21 16:14:02 +0300

    react_movies-list-filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,48 @@
-import React from 'react';
-import './App.scss';
-import { MoviesList } from './components/MoviesList';
-import moviesFromServer from './api/movies.json';
+import React, { useState } from "react";
+
+import "./App.scss";
+import { MoviesList } from "./components/MoviesList";
+import moviesFromServer from "./api/movies.json";
+
+type Movie = {
+  title: string;
+  description: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
+};
 
 export const App: React.FC = () => {
+  const [visibleMovies, setVisibleMovies] = useState<Movie[]>(moviesFromServer);
+
+  const filteredMovies = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+
+    if (!value.trim()) {
+      setVisibleMovies(moviesFromServer);
+    }
+
+    const filteredMovies = moviesFromServer.filter(
+      (movie: Movie) =>
+        movie.title
+          .toLocaleLowerCase()
+          .includes(
+            value
+            .toLocaleLowerCase()
+            .trim()
+            ) ||
+        movie.description
+          .toLocaleLowerCase()
+          .includes(
+            value
+            .toLocaleLowerCase()
+            .trim()
+            )
+    );
+
+    setVisibleMovies(filteredMovies);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,17 +59,16 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                onChange={filteredMovies}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <div className="sidebar">
-        Sidebar goes here
-      </div>
+      <div className="sidebar">Sidebar goes here</div>
     </div>
   );
 };
